effa5221d6270feee52d86dca17a44f7461b2a64
Pratigya7 <pratigya2061@gmail.com>
2024-07-21 17:15:00 +0545

    making protfolio webside

---
+++ my protolio website/main.js
@@ -0,0 +1,9 @@
+function menu(){
+    const home=$(active).val()
+    const about=$(actve).val()
+    const contact=$(active).val()
+
+    console.log(home);
+    console.log(about);
+    console.log(contact);
+}
