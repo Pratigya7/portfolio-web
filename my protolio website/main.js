7ca99e51e87d746de60f5f7842eaffd1a81e4172
Pratigya7 <pratigya2061@gmail.com>
2024-07-29 17:24:28 +0545

    change background.png to background1.png

--- my protolio website/main.js
+++
@@ -1,9 +0,0 @@
-function menu(){
-    const home=$(active).val()
-    const about=$(actve).val()
-    const contact=$(active).val()
-
-    console.log(home);
-    console.log(about);
-    console.log(contact);
-}
